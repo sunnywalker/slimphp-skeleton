da4ad6942b63ce660a2f5ee4563f966c63a89f7f
Sunny Walker <swalker@hawaii.edu>
2017-10-18 08:51:59 -1000

    add log-in, log-out routes; use namespaces

--- app/routes/web-admin.php
+++ app/routes/web-admin.php
@@ -1,11 +1,20 @@
 <?php
+use App\Controllers\Admin\AuthNController;
+use App\Controllers\Admin\HomeAdminController;
+use App\Controllers\Admin\UserAdminController;
+use App\Middleware\AuthNMiddleware;
+
+$app->get('/admin/log-in', AuthNController::class.':index')->setName('log-in');
+$app->post('/admin/log-in', AuthNController::class.':logIn');
+$app->get('/admin/log-out', AuthNController::class.':logOut')->setName('log-out');
+
 $app->group('/admin', function () {
-    $this->get('/', App\Controllers\HomeAdminController::class.':index')->setName('admin-home');
+    $this->get('/', HomeAdminController::class.':index')->setName('admin-home');
 
     $this->group('/users', function () {
-        $this->get('/', App\Controllers\UserAdminController::class.':index')->setName('admin-users');
-        $this->post('/', App\Controllers\UserAdminController::class.':create');
-        $this->get('/{id:[0-9]+}', App\Controllers\UserAdminController::class.':edit');
-        $this->post('/{id:[0-9]+}', App\Controllers\UserAdminController::class.':submit');
+        $this->get('/', UserAdminController::class.':index')->setName('admin-users');
+        $this->post('/', UserAdminController::class.':create');
+        $this->get('/{id:[0-9]+}', UserAdminController::class.':edit');
+        $this->post('/{id:[0-9]+}', UserAdminController::class.':submit');
     });
-})->add(new App\Middleware\AuthNMiddleware($container));
+})->add(new AuthNMiddleware($container));
